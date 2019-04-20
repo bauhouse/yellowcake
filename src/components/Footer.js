0b1ab6dd48a349a553ca46e77792dbe75197775d
Stephen Bau <bauhouse@gmail.com>
2019-04-20 13:04:22 -0700

    Add logo to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import InstagramFeed from './InstagramFeed'
 import './Footer.css'
+import { Link } from 'gatsby'
+import LogoFooter from './LogoFooter'
 
 export default () => (
   <div>
@@ -12,6 +14,11 @@ export default () => (
     <InstagramFeed count="8" />
     <footer className="footer">
       <div className="container taCenter">
+        <div class="brand">
+          <Link to="/">
+            <LogoFooter/>
+          </Link>
+        </div>
         <span>
           The <a href="https://fraservalleyrowing.com/">Fraser Valley Rowing Club</a> acknowledges the financial assistance of the Province of British Columbia.
         </span>
