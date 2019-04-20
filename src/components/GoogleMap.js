7d2b98d660875dcc6242b69b8d99e5e0608ad9d6
Stephen Bau <bauhouse@gmail.com>
2019-04-20 12:45:47 -0700

    Update Google Map component

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -10,8 +10,8 @@ if (process.env.NETLIFY_MAP_KEY) {
 class GoogleMap extends Component {
   static defaultProps = {
     center: {
-      lat: -28.0914483,
-      lng: 153.4425208
+      lat: 49.11153,
+      lng: -122.11471
     },
     zoom: 14
   }
@@ -25,7 +25,7 @@ class GoogleMap extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-          <Marker lat={-28.0914483} lng={153.4425208} text={'Kreyser Avrora'} />
+          <Marker lat={49.11153} lng={-122.11471} text={'Fraser Valley Rowing Club'} />
         </GoogleMapReact>
       </div>
     )
