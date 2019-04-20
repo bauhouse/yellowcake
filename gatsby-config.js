5e1b0a4d076303fd5843b1ee2574594159b1c8a7
Stephen Bau <bauhouse@gmail.com>
2019-04-20 10:29:41 -0700

    Modify gatsby configuration for siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,8 @@ const postcssPresetEnv = require('postcss-preset-env')
 
 module.exports = {
   siteMetadata: {
-    title: 'Yelloecake',
-    siteUrl: 'https://yellowcake.netlify.com'
+    title: 'Fraser Valley Rowing Club',
+    siteUrl: 'https://fvrc.netlify.com'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
